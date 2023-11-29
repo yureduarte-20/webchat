b25565bde4611994299a7155073ef154aff0feb6
Yure Samarone Gomes Duarte <yuresamarone34@gmail.com>
2023-11-29 01:16:20 -0300

    contact controller

--- src/controllers/contact.controller.ts
+++ src/controllers/contact.controller.ts
@@ -4,8 +4,8 @@ import { RequestUserProfile } from "../middlewares/auth.middleware";
 import ContactRepository from "../repositories/ContactsRepository";
 import DefaultRepository from "../repositories/DefaultRepository";
 import { Contact } from "../entity/Contact";
-import { UserRepository } from "../repositories";
-import { EntityNotFoundError, FindManyOptions, FindOneOptions } from "typeorm";
+import { UserNotFoundError, UserRepository } from "../repositories";
+import { FindOneOptions } from "typeorm";
 import { User } from "../entity";
 import { json } from "body-parser";
 
@@ -22,14 +22,19 @@ export class ContactController {
 
     async create(request : RequestUserProfile, response: Response){
         const { contactEmail } = request.body;
-        const search : FindOneOptions<User> = { where:{ email: contactEmail } }
+        const search : FindOneOptions<User> = { where:{ email: contactEmail }, relationLoadStrategy: 'join', relations:[] }
         try{
             const userContact = await this.userRepository.findOne(search)
             const searchContatc: FindOneOptions<Contact> = { where: { destinationUserId: userContact.id, userId: request.user.id } }
-            const contact = await this.contactRepository.findOne(searchContatc)
-            if(contact) return response.status(422).json({ message: 'Você já tem esse contato cadastrado' })
+            const contactExists = await this.contactRepository.findOne(searchContatc)
+            if(contactExists) return response.status(422).json({ message: 'Você já tem esse contato cadastrado' })
+            let contact = new Contact()
+            contact.destinationUserId = userContact.id
+            contact.userId = request.user.id
+            contact = await this.contactRepository.save(contact);
+            return response.status(201).json(contact)
         } catch(e){
-            if(e instanceof EntityNotFoundError){
+            if(e instanceof UserNotFoundError){
                 return response.status(404).json({ message: e.message })
             }
             return response.status(500).json({ message: 'Erro no servidor' })
